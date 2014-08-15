ca2dd1b53b95245881cd308448cdd50a3a916a50
Saša Barišić <cartman300@net.hr>
2014-08-15 21:44:59 +0200

    Random, Simplex, Screen

---
+++ Libraria/Libraria/Random.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraria {
+	public static class Rand {
+		public static Random R = new Random();
+	}
+}
