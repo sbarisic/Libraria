edfe4f1a38a49ad4e2ee7d2e81869959e8b2d605
Saša Barišić <cartman300@net.hr>
2016-02-24 17:53:18 +0100

    patterns

--- Libraria/LibTests/Program.cs
+++ Libraria/LibTests/Program.cs
@@ -8,17 +8,14 @@ using System.Reflection;
 using System.Reflection.Emit;
 
 using Libraria;
-using Libraria.Reflection;
-using Libraria.Interop;
+using Libraria.Patterns;
 
 namespace LibTests {
 	public class Program {
 		static void Main(string[] args) {
 			Console.Title = "LibTests";
 
-			dynamic Native = new NativeBinder(CallingConvention.Winapi);
-			Native.Kernel32.Beep((uint)2000, (uint)500);
-			Native.User32.MessageBoxA(IntPtr.Zero, "Text", "Caption", 0);
+
 
 			Console.WriteLine("Done!");
 			Console.ReadLine();
