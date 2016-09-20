c60bef5338f3c329a680202c1ae8c81df3ba7fab
Saša Barišić <cartman300@net.hr>
2016-09-20 15:03:29 +0200

    Added hacks and test

---
+++ Libraria.Hacks/Zero2Hero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Port of https://github.com/massivedynamic/zero2hero
+
+namespace Libraria.Hacks {
+	public static class Zero2Hero {
+
+		public static void ExploitUAC() {
+
+		}
+	}
+}
