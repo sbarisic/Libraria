697bade910bb7290a90cb1ad44f8fc1ca2d31019
Saša Barišić <cartman300@net.hr>
2017-02-20 09:21:56 +0100

    Added some native stuff

--- Libraria.Native/Kernel32.cs
+++ Libraria.Native/Kernel32.cs
@@ -4,6 +4,21 @@ using System.Text;
 using System.Runtime.InteropServices;
 
 namespace Libraria.Native {
+	[Flags]
+	public enum MemProtection : uint {
+		NoAccess = 0x01,
+		ReadOnly = 0x02,
+		ReadWrite = 0x04,
+		WriteCopy = 0x08,
+		Exec = 0x10,
+		ExecRead = 0x20,
+		ExecReadWrite = 0x40,
+		ExecWriteCopy = 0x80,
+		PageGuard = 0x100,
+		NoCache = 0x200,
+		WriteCombine = 0x400
+	}
+
 	public static class Kernel32 {
 		[DllImport("kernel32")]
 		public static extern bool AllocConsole();
@@ -17,6 +32,28 @@ namespace Libraria.Native {
 
 		[DllImport("kernel32", SetLastError = true)]
 		public static extern bool VirtualFree(IntPtr Addr, IntPtr Size, AllocationType FreeType);
+		
+		[DllImport("kernel32", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+		public static extern IntPtr GetProcAddress(IntPtr Lib, string ProcName);
+
+		[DllImport("kernel32", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+		public static extern IntPtr GetModuleHandle(string ModuleName);
+
+		[DllImport("kernel32", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+		public static extern bool VirtualProtect(IntPtr Addr, uint Size, MemProtection NewProtect, out MemProtection OldProtect);
+
+		public static bool VirtualProtect(IntPtr Addr, int Size, MemProtection NewProtect, out MemProtection OldProtect) {
+			return VirtualProtect(Addr, (uint)Size, NewProtect, out OldProtect);
+		}
+
+		public static bool VirtualProtect(IntPtr Addr, uint Size, MemProtection NewProtect) {
+			MemProtection Old;
+			return VirtualProtect(Addr, Size, NewProtect, out Old);
+		}
+
+		public static bool VirtualProtect(IntPtr Addr, int Size, MemProtection NewProtect) {
+			return VirtualProtect(Addr, (uint)Size, NewProtect);
+		}
 
 		public static bool VirtualFree(IntPtr Addr) {
 			return VirtualFree(Addr, IntPtr.Zero, AllocationType.RELEASE);
---
+++ Libraria.Native/MemoryManagement.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libraria.Native {
+	/// <summary>
+	/// Provides memory management convenience functions.
+	/// </summary>
+	public static class MemoryManagement {
+		/// <summary>
+		/// <para>A handle to safely temporarily change memory protection.</para>
+		/// <para>The protection is reset when the handle is disposed.</para>
+		/// </summary>
+		public class VirtualProtectHandle : IDisposable {
+			/// <summary>
+			/// The address of the first byte that had its protection changed.
+			/// </summary>
+			public IntPtr Address {
+				get;
+				private set;
+			}
+
+			/// <summary>
+			/// The size of the memory range with changed protection.
+			/// </summary>
+			public int Size {
+				get;
+				private set;
+			}
+
+			/// <summary>
+			/// The previous protection setting.
+			/// </summary>
+			public MemProtection OldProtection {
+				get;
+				private set;
+			}
+
+			/// <summary>
+			/// Creates a new <see cref="VirtualProtectHandle"/> instance with the given parameters.
+			/// </summary>
+			/// <param name="address">The address of the first byte that had its protection changed.</param>
+			/// <param name="size">The size of the memory range with changed protection.</param>
+			/// <param name="oldProtection">The previous protection setting.</param>
+			public VirtualProtectHandle(IntPtr address, int size, MemProtection oldProtection) {
+				Address = address;
+				Size = size;
+				OldProtection = oldProtection;
+			}
+
+			/// <summary>
+			/// Resets the memory protection to its previous value.
+			/// </summary>
+			public void Dispose() {
+				Kernel32.VirtualProtect(Address, Size, OldProtection);
+			}
+		}
+
+		public static VirtualProtectHandle Protect(IntPtr address, int size, MemProtection newProtection) {
+			MemProtection oldProtection;
+			Kernel32.VirtualProtect(address, size, newProtection, out oldProtection);
+			return new VirtualProtectHandle(address, size, oldProtection);
+		}
+	}
+}
